709f8b0f2f79162ec60a16c3cb3d365d836b4cd8
Bazelon-ai <wendybazelon@outlook.com>
2026-01-12 11:58:17 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Docs Help Comcast'
+project = 'KB Docs Comcast'
 copyright = '2025'
-author = 'Drew Barzola'
+author = 'Dotty Attie'
 
 release = '1.0'
 
@@ -21,3 +21,4 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
